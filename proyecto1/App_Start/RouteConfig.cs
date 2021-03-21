f4a505a27cf4bd4bc3fb40d656b51415ecce1053
Jorge Meza Murillo <mezamjorge@hotmail.com>
2021-03-20 23:37:46 -0500

    Definiendo el controlador en el menu

--- proyecto1/App_Start/RouteConfig.cs
+++ proyecto1/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace proyecto1
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Rol", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
