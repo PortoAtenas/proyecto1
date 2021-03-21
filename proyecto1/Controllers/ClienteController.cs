4bffe1c60b988e8e9a60354e745e0094976c26ed
Jorge Meza Murillo <mezamjorge@hotmail.com>
2021-03-21 13:18:57 -0500

    Listar los clientes y agregar al menu

---
+++ proyecto1/Controllers/ClienteController.cs
@@ -0,0 +1,107 @@
+﻿using proyecto1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace proyecto1.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            List<ClienteCLS> listaCliente = null;
+
+            using (var bd = new proyectoSEntities1())
+            {
+                listaCliente = (from Client in bd.Cliente
+                                select new ClienteCLS
+                                {
+                                    icedula = Client.cedula,
+                                    inombreC = Client.nombreC,
+                                    iapellidosC = Client.apellidos,
+                                    idireccion = Client.direccion,
+                                    icelular = Client.celular,
+                                    itelefono = Client.telefono
+                                }).ToList();
+            }
+
+
+            return View(listaCliente);
+        }
+
+        // GET: Cliente/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Cliente/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cliente/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Cliente/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Cliente/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Cliente/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Cliente/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
