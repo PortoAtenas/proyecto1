b01751b212bc5bbecb10baa15fe32038e4550033
Jorge Meza Murillo <mezamjorge@hotmail.com>
2021-03-21 11:48:06 -0500

    Cambios para poder listar los productos ademas de agregarlo al menu

---
+++ proyecto1/Models/ProductoCLS.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace proyecto1.Models
+{
+    public class ProductoCLS
+    {
+        [Display(Name = "Id Producto")]
+        public int iidProducto { get; set; }
+
+        [Display(Name = "Nombre Producto")]
+        public string nombreP { get; set; }
+
+        [Display(Name = "Precio")]
+        public double precio { get; set; }
+
+        [Display(Name = "Coste")]
+        public double coste { get; set; }
+
+
+        [Display(Name ="Descripcion")]
+        public string descripcion { get; set; }
+
+
+        [Display(Name ="Cantidad Stock")]
+        public int cantidad { get; set; } 
+
+
+        [Display(Name ="Estado del producto")]
+        public int iidState { get; set; }
+
+    }
+}
